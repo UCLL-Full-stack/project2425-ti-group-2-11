75f1c28861bd11fd834c5874c86fd4cbc1c3aa37
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-10-17 13:40:47 +0200

    Review model toegevoegd

---
+++ back-end/model/review.ts
@@ -0,0 +1,37 @@
+import { User } from './user';
+
+export class Review {
+    private id?: number;
+    private user: User;
+    private product: Product;
+    private reviewText: string;
+
+    constructor(review: {id?: number; user: User; product: Product; reviewText: string }) {
+        this.id = review.id;
+        this.user = review.user;
+        this.product = review.product;
+        this.reviewText = review.reviewText;
+    }
+
+    getId(): number | undefined{
+        return this.id;
+    }
+    getUser(): User {
+        return this.user;
+    }
+    getProduct(): Product {
+        return this.product;
+    }
+    getReviewText(): string {
+        return this.reviewText;
+    }
+
+
+    equals(review: Review): boolean {
+        return (
+            this.user == review.user &&
+            this.product == review.product &&
+            this.reviewText == review.reviewText
+        );
+    }
+}
