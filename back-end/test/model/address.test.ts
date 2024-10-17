47cf9685d39b8c2f3abdecade76dde08eec5e336
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-10-17 14:20:05 +0200

    added test for address equal function

--- back-end/test/model/address.test.ts
+++ back-end/test/model/address.test.ts
@@ -1,3 +1,4 @@
+import exp from "constants";
 import { Address } from "../../model/address"
 
 const VALID_STREET = 'Bondgenotenlaan';
@@ -16,3 +17,10 @@ test('given: valid address info, when: creating address, then: address is create
     expect(address.getPostalCode()).toEqual(VALID_POSTALCODE);
     expect(address.getCountry()).toEqual(VALID_COUNTRY);
 });
+
+test('given: two identical addresses, when: asking if equal, then: true is returned', () => {
+    const address1 = new Address({ street: VALID_STREET, city: VALID_CITY, state: VALID_STATE, postalCode: VALID_POSTALCODE, country: VALID_COUNTRY })
+    const address2 = new Address({ street: VALID_STREET, city: VALID_CITY, state: VALID_STATE, postalCode: VALID_POSTALCODE, country: VALID_COUNTRY })
+
+    expect(address1.equals(address2)).toBe(true);
+})
