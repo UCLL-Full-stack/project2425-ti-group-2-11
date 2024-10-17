3f43ff3f9b05b5fcbc9f41a9595f45c21fa97b88
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-10-17 13:41:44 +0200

    Deze keer met import van product

--- back-end/model/review.ts
+++ back-end/model/review.ts
@@ -1,4 +1,5 @@
 import { User } from './user';
+import { Product } from './product';
 
 export class Review {
     private id?: number;
@@ -6,14 +7,14 @@ export class Review {
     private product: Product;
     private reviewText: string;
 
-    constructor(review: {id?: number; user: User; product: Product; reviewText: string }) {
+    constructor(review: { id?: number; user: User; product: Product; reviewText: string }) {
         this.id = review.id;
         this.user = review.user;
         this.product = review.product;
         this.reviewText = review.reviewText;
     }
 
-    getId(): number | undefined{
+    getId(): number | undefined {
         return this.id;
     }
     getUser(): User {
@@ -25,7 +26,6 @@ export class Review {
     getReviewText(): string {
         return this.reviewText;
     }
-
 
     equals(review: Review): boolean {
         return (
