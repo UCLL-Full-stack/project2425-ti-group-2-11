a9ec01ddc82353ba80006f1f1f5186e35b9761d3
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-10-17 13:22:55 +0200

    address model + test

---
+++ back-end/model/address.ts
@@ -0,0 +1,65 @@
+export class Address {
+    private id: number | undefined;
+    private street: string;
+    private city: string;
+    private state: string;
+    private postalCode: string;
+    private country: string;
+
+    constructor(address: { street: string, city: string, state: string, postalCode: string, country: string, id?: number | undefined }) {
+        this.id = address.id;
+        this.street = address.street;
+        this.city = address.city;
+        this.state = address.state;
+        this.postalCode = address.postalCode;
+        this.country = address.country;
+    };
+
+    public getId(): number | undefined {
+        return this.id;
+    };
+
+    public getStreet(): string {
+        return this.street;
+    }
+
+    public getCity(): string {
+        return this.city;
+    }
+
+    public getState(): string {
+        return this.state;
+    }
+
+    public getPostalCode(): string {
+        return this.postalCode;
+    }
+
+    public getCountry(): string {
+        return this.country;
+    }
+
+    // public setId(id: number): void {
+    //     this.id = id;
+    // }
+
+    // public setStreet(street: string): void {
+    //     this.street = street;
+    // }
+
+    // public setCity(city: string): void {
+    //     this.city = city;
+    // }
+
+    // public setState(state: string): void {
+    //     this.state = state;
+    // }
+
+    // public setPostalCode(postalCode: string): void {
+    //     this.postalCode = postalCode;
+    // }
+
+    // public setCountry(country: string): void {
+    //     this.country = country;
+    // }
+}
---
+++ back-end/test/model/address.test.ts
@@ -0,0 +1,18 @@
+import { Address } from "../../model/address"
+
+const VALID_STREET = 'Bondgenotenlaan';
+const VALID_CITY = 'Leuven';
+const VALID_STATE = 'Leuven';
+const VALID_POSTALCODE = '3000';
+const VALID_COUNTRY = 'Belgium';
+
+
+test('given: valid address info, when: creating address, then: address is created', () => {
+    const address = new Address({ street: VALID_STREET, city: VALID_CITY, state: VALID_STATE, postalCode: VALID_POSTALCODE, country: VALID_COUNTRY })
+
+    expect(address.getStreet()).toEqual(VALID_STREET);
+    expect(address.getCity()).toEqual(VALID_CITY);
+    expect(address.getState()).toEqual(VALID_STATE);
+    expect(address.getPostalCode()).toEqual(VALID_POSTALCODE);
+    expect(address.getCountry()).toEqual(VALID_COUNTRY);
+});
