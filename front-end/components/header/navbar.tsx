f77a42ca62fb377acc96fe49c4262eccedd08772
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-11-02 14:31:35 +0100

    Tailwind geinstalleerd voor css, begonnen met navbar en onnodige dingen? aangepast? Ook logo gemaakt

---
+++ front-end/components/header/navbar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="l:flex l:flex-row pb-5 pt-5">
+      <div className="flex justify-between	">
+        <img
+          src="/logo-512.svg"
+          alt="Logo User Bazaar"
+          className="w-2/12 mr-5 ml-5"
+        />
+        <div className="flex mr-5 justify-end">
+          <img
+            src="/google-person.svg"
+            alt="Person Icon link to profile"
+            className="w-2/12"
+          />
+          <img
+            src="/google-shopping-cart.svg"
+            alt="Person Icon link to shopping cart"
+            className="w-2/12"
+          />
+        </div>
+      </div>
+      <div className="flex place-content-center w-full mt-4">
+        <div className="relative w-full ml-5 mr-5">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-full p-5 pr-16 border rounded-full text-2xl"
+          />
+          <div className="absolute inset-y-0 right-4 flex items-center pointer-events-none">
+            <span className="material-icons text-gray-500 text-2xl">
+              search
+            </span>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </nav>
+  );
+};
+
+export default Navbar;
