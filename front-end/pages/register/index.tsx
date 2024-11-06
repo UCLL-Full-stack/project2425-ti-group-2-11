5a5459157d5a44d6396c38cfc177ca388ddf5682
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-11-06 16:41:37 +0100

    added register form to page

---
+++ front-end/pages/register/index.tsx
@@ -0,0 +1,13 @@
+import RegisterForm from "@/components/register/registerForm"
+
+const Register: React.FC = () => {
+    return (
+        <main className="flex justify-center items-center h-screen">
+            <div>
+                <RegisterForm />
+            </div>
+        </main>
+    );
+}
+
+export default Register;
