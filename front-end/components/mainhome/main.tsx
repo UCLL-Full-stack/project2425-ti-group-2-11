603ac6f08e679302688ccd4402a092162e062e30
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-11-14 22:31:29 +0100

    main domain aangepast zodat pickers erin zitten

--- front-end/components/mainhome/main.tsx
+++ front-end/components/mainhome/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
+import CategoriePicker from "@/components/mainhome/categoriePicker";
+import ProductPicker from "./productPicker";
 
 const Main: React.FC = () => {
-    return(
-        <>
-        <p>test</p>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="h-svh ml-5 mr-5">
+        <CategoriePicker />
+        <ProductPicker />
+      </div>
+    </>
+  );
+};
 
 export default Main;
-
-
