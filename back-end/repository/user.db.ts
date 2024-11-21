0d665bfda3a976dbd8babb0848afd80e519c41d6
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-11-21 14:31:49 +0100

    changed to db instead of list

--- back-end/repository/user.db.ts
+++ back-end/repository/user.db.ts
@@ -1,59 +1,33 @@
-import { UserInfo } from 'os';
 import { Address } from '../model/address';
 import { User } from '../model/user';
 import { UserInput } from '../types';
 import database from './database';
-const users: User[] = [];
 
-const user1: User = new User({
-    id: 1,
-    name: 'Robin Ghys',
-    phoneNumber: '0476577258',
-    emailAddress: 'r0997165@ucll.be',
-    password: 'password',
-    address: new Address({
-        id: 1,
-        street: 'Kerkstraat',
-        houseNumber: '4',
-        city: 'Leuven',
-        state: 'Vlaams-Brabant',
-        postalCode: '3000',
-        country: 'Belgium',
-    }),
-    seller: false,
-    newsLetter: true,
-    role: 'user',
-});
 
-const user2: User = new User({
-    id: 2,
-    name: 'Tuur Klaasen',
-    phoneNumber: '0499999999',
-    emailAddress: 'r0997166@ucll.be',
-    password: 'wachtwoord',
-    address: new Address({
-        id: 1,
-        street: 'bondgenotenlaan',
-        houseNumber: '1',
-        city: 'Leuven',
-        state: 'Vlaams-Brabant',
-        postalCode: '3000',
-        country: 'Belgium',
-    }),
-    seller: true,
-    newsLetter: true,
-    role: 'admin',
-});
-
-users.push(user1);
-users.push(user2);
-
-const getAllUsers = (): User[] => {
-    return users;
+const getAllUsers = async (): Promise<User[]> => {
+    try {
+        const UserPrisma = await database.user.findMany({
+            include: { address: true }
+        });
+        return UserPrisma.map((UserPrisma) => User.from(UserPrisma));
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database error. See server log for details.');
+    }
 };
 
-const getUserById = (id: number): User | undefined => {
-    return users.find((user) => user.getId() === id);
+const getUserById =  async ({ id }: { id: number }): Promise<User | null> => {
+    try {
+        const userPrisma = await database.user.findUnique({
+            where: { id },
+            include: {address: true}
+        });
+
+        return userPrisma ? User.from(userPrisma) : null;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database error. See server log for details.');
+    }
 };
 
 const addUser = async (user: User): Promise<User> => {
