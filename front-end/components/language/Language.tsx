9ad8638f031fd622ad8e516d6918a831a5f4156a
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-12-05 14:52:05 +0100

    added translations

--- front-end/components/language/Language.tsx
+++ front-end/components/language/Language.tsx
@@ -1,8 +1,10 @@
+import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 
 const Language: React.FC = () => {
   const router = useRouter();
   const { locale, pathname, asPath, query } = router;
+  const { t } = useTranslation();
 
   const handleLanguageChange = (event: { target: { value: string } }) => {
     // get new locale from event and push it to the router
@@ -18,13 +20,13 @@ const Language: React.FC = () => {
   };
 
   return (
-    <div className="ml-6">
-      <label htmlFor="language" className="text-white">
-        Language
+    <div>
+      <label htmlFor="language" className="text-black justify-start">
+        {t('language')}
       </label>
       <select
         id="language"
-        className="ml-2 p-1"
+        className="ml-2 p-1 rounded-lg"
         value={locale}
         onChange={handleLanguageChange}
       >
