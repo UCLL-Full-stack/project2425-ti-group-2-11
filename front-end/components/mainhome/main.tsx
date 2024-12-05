c7785d2517002cdb2e9f699b82f7323211906b1a
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-12-05 14:52:30 +0100

    added loginTable component

--- front-end/components/mainhome/main.tsx
+++ front-end/components/mainhome/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CategoriePicker from "@/components/mainhome/categoriePicker";
 import ProductPicker from "./productPicker";
+import LoginTable from "../loginTable/loginTable";
 
 const Main: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ const Main: React.FC = () => {
       <div className="h-svh ml-5 mr-5">
         <CategoriePicker />
         <ProductPicker />
+        <LoginTable />
       </div>
     </>
   );
