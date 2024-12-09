13acf7861e3fad3e1615bd2c7d772d2416c06648
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-12-09 18:35:53 +0100

    translation aan index pagina

--- front-end/components/login/loginForm.tsx
+++ front-end/components/login/loginForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "../header/navbar";
 import Image from "next/image";
+import { useTranslation } from "next-i18next";
 
 interface LoginFormData {
   name: string;
@@ -8,9 +9,12 @@ interface LoginFormData {
 }
 
 const LoginForm: React.FC = () => {
+
+  const { t } = useTranslation();
+
   const [formData, setFormData] = useState<LoginFormData>({
-    name: "",
-    password: "",
+    name: '',
+    password: '',
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
@@ -30,23 +34,23 @@ const LoginForm: React.FC = () => {
   return (
     <>
       <div className="w-1/3 h-1/2 flex flex-col items-center justify-center">
-        <h1 className="font-bold text-3xl">Login</h1>
+        <h1 className="font-bold text-3xl">{t("login.login")}</h1>
         <form action="login" className="flex flex-col pt-10" onSubmit={handleSubmit}>
-          <label htmlFor="email">email:</label>
+          <label htmlFor="email">{t("login.email")}:</label>
           <input
             type="text"
-            placeholder="email"
+            placeholder={t("login.email")}
             id="email"
             required
             className="p-2"
             name="email"
             onChange={handleChange}
           />
-          <label htmlFor="password">password:</label>
+          <label htmlFor="password">{t("login.password")}:</label>
           <input
             type="password"
             id="password"
-            placeholder="password"
+            placeholder={t("login.password")}
             required
             className="p-2"
             name="password"
@@ -55,8 +59,8 @@ const LoginForm: React.FC = () => {
           <input
             type="submit"
             id="submit"
-            value="Login"
-            className="mt-5 bg-white max-w-12 p-1 self-center hover:cursor-pointer hover:bg-gray-600"
+            value={t("login.login")}
+            className="mt-5 bg-white p-1 self-center hover:cursor-pointer hover:bg-gray-600"
           />
         </form>
         <p
@@ -65,7 +69,7 @@ const LoginForm: React.FC = () => {
             window.location.href = "/register";
           }}
         >
-          No account? Register here
+          {t("login.NoAccount")}
         </p>
       </div>
     </>
