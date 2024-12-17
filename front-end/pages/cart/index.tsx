d632982b6f41911727fe80c2542e347c2cb25da3
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-12-17 14:50:46 +0100

    i18n

--- front-end/pages/cart/index.tsx
+++ front-end/pages/cart/index.tsx
@@ -1,9 +1,30 @@
 import Navbar from "@/components/header/navbar";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Cart: React.FC = () => {
-  return <>
-    <Navbar />
-  </>;
-};
+  const router = useRouter();
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+    }
+  }, [router]);
+  return (
+    <>
+      <Navbar />
+      <main>{/* <ShoppingCart shoppingCartId="123" /> */}</main>
+    </>
+  );
+};
+export const getServerSideProps = async (context: { locale: any }) => {
+  const { locale } = context;
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? "en", [`common`])),
+    },
+  };
+};
 export default Cart;
--- front-end/pages/profile/index.tsx
+++ front-end/pages/profile/index.tsx
@@ -1,9 +1,29 @@
 import Navbar from "@/components/header/navbar";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Profile: React.FC = () => {
-  return <>
-    <Navbar />
-  </>;
-};
+  const router = useRouter();
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+    }
+  }, [router]);
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+export const getServerSideProps = async (context: { locale: any; }) => {
+  const { locale } = context;
+  return {
+      props: {
+          ...(await serverSideTranslations(locale ?? "en", [`common`])),
+      },
+  };
+}
 export default Profile;
