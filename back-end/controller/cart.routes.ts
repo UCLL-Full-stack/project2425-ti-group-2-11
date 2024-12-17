6e9f0b938c1442bcb671220cbaceff2ea811e2b7
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-12-17 22:45:48 +0100

    cartrouter export

--- back-end/controller/cart.routes.ts
+++ back-end/controller/cart.routes.ts
@@ -3,10 +3,13 @@ import cartService from '../service/cart.service';
 
 const cartRouter = express.Router();
 
-cartRouter.get('/items:cartId', async (req: Request, res: Response, next: NextFunction) => {
+cartRouter.get('/items/:userId', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.send(await cartService.getCartItems(Number(req.params.cartId)));
+        res.send(await cartService.getCartItems(Number(req.params.userId)));
     } catch (error) {
         next(error);
     }
 });
+
+
+export default cartRouter;
