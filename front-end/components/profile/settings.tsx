f8f332a92bae69178732b7691ae9a90fc5d9a735
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-12-18 20:19:46 +0100

    made a settings page

--- front-end/components/profile/settings.tsx
+++ front-end/components/profile/settings.tsx
@@ -1,7 +1,75 @@
+import { useTranslation } from "next-i18next";
+import Language from "../language/Language";
+import UserService from "@/services/UserService";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import { Address, Role } from "@/types/types";
+import AnimatedCheckbox from "../register/AnimatedCheckbox";
+
+
+interface ProfileProps {
+    userId?: number;
+    name: string;
+    phoneNumber: string;
+    emailAddress: string;
+    password: string;
+    address: Address;
+    seller: boolean;
+    newsLetter: boolean;
+    role: Role;
+}
+
+
 const Settings: React.FC = () => {
+    const { t } = useTranslation();
+    const [userId, setUserId] = useState<number>()
+    const [user, setUser] = useState<any>(null);
+    const [seller, setSeller] = useState<boolean>(false);
+
+    const fetchUser = async (userId: number) => {
+        try {
+            const user = await UserService.getUser(userId);
+            setUser(user);
+        } catch (error) {
+            console.error('Failed to fetch user data:', error);
+        }
+    };
+
+    useEffect(() => {
+        const fetchToken = async () => {
+            const token = localStorage.getItem('token');
+            if (token) {
+                const decoded = jwtDecode<ProfileProps>(token);
+                if (decoded && decoded.userId) {
+                    setUserId(decoded.userId);
+                    fetchUser(decoded.userId);
+                }
+            }
+        }
+        fetchToken()
+    }, [])
+
+    useEffect(() => {
+        if (user !== null) {
+            setSeller(user.seller);
+            console.log(user.seller);
+        }
+    }, [user]);
+
+    const updateSeller = () => {}
+
     return (
         <>
-            Settings
+            <div className="flex flex-row items-center">{t('language')}: <Language /></div>
+            <div className="flex flex-row items-center gap-2 pt-2">
+            {t("register.sellercheckbox")}
+            <AnimatedCheckbox
+                label={""}
+                name="seller"
+                onchange={() => updateSeller()}
+                checked={seller}
+            />
+            </div>
         </>
     )
 };
