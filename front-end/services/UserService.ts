597ca220e3d472582cdadaa315aa0dcad04b3e5c
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-12-18 18:00:17 +0100

    created requests to get a user

--- front-end/services/UserService.ts
+++ front-end/services/UserService.ts
@@ -10,8 +10,27 @@ const loginUser = (user: User) => {
     });
 };
 
+const getUser = async (userId: number) => {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + `users/${userId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+
+    const user = await response.json();
+    return user;
+};
+
 const UserService = {
     loginUser,
+    getUser,
 };
+
 
 export default UserService;
