1c2e05b11fb0e868725beaeab3ff722ed712a6db
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-12-19 02:25:10 +0100

    deze mocht niet weg whoops

--- front-end/pages/cart/index.tsx
+++ front-end/pages/cart/index.tsx
@@ -30,7 +30,13 @@ const Cart: React.FC = () => {
       }
     }
   }, [router]);
-
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-64">
+        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    );
+  }
   return (
     <>
       <Head>
