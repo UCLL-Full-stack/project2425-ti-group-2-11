f0d40d8eb6186be07e0c82052c15febe8b2b576c
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-12-19 18:24:18 +0100

    + tooltip

--- front-end/components/profile/options/admin.tsx
+++ front-end/components/profile/options/admin.tsx
@@ -2,7 +2,8 @@ import { User } from "@/types/types";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import { getAllUsers } from "@/services/UserService";
-import { Trash } from "lucide-react";
+import { Store, Trash } from "lucide-react";
+import Tooltip from "@mui/material/Tooltip";
 
 const Admin: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
@@ -18,24 +19,56 @@ const Admin: React.FC = () => {
     };
     loadUsers();
   }, [users]);
+  const handleRevokeSeller = (userId: number) => async () => {
+    console.log("Revoke seller", userId);
+  };
+  const handleGrantSeller = (userId: number) => async () => {
+    console.log("Grant seller", userId);
+  };
+  const handleDeleteuser = (userId: number) => async () => {
+    console.log("Delete", userId);
+  };
   if (!users) {
     return <p>Loading...</p>;
   } else {
     return (
       <>
         <div>
-          <h1 className="mb-3">Admin</h1>
+          <h1 className="mb-3">All users</h1>
         </div>
         <div className="w-full flex flex-col gap-3">
           {users.map((user) => (
             <section
-              className="flex justify-between bg-white shadow-md py-2 px-2"
+              className="flex rounded justify-between bg-white shadow-md py-2 px-2"
               key={user.id}
             >
               <p>{user.name}</p>
-              <button>
-                <Trash />
-              </button>
+              <div className="flex gap-5">
+                {user.seller ? (
+                  <Tooltip
+                    title={`Revoke ${user.name}'s "Seller" status`}
+                    placement="bottom"
+                  >
+                    <button onClick={handleRevokeSeller(user.id)}>
+                      <Store className="text-red-500" />
+                    </button>
+                  </Tooltip>
+                ) : (
+                  <Tooltip
+                    title={`Grant ${user.name} "Seller" status`}
+                    placement="bottom"
+                  >
+                    <button onClick={handleGrantSeller(user.id)}>
+                      <Store className="text-green-500" />
+                    </button>
+                  </Tooltip>
+                )}
+                <Tooltip title={`Delete ${user.name}`} placement="bottom">
+                  <button onClick={handleDeleteuser(user.id)}>
+                    <Trash className="text-red-500" />
+                  </button>
+                </Tooltip>
+              </div>
             </section>
           ))}
         </div>
