b18b5ff5c1a28aed72d280524160951f4d7037bb
RobinGhysUCLL <robin.ghys@student.ucll.be>
2024-12-19 15:09:36 +0100

    popup message bij all products

--- front-end/components/main/AllProducts.tsx
+++ front-end/components/main/AllProducts.tsx
@@ -3,11 +3,15 @@ import { ProductCard } from "./ProductCard";
 
 interface AllProductsProps {
   items: Product[];
+  onMessage: (text: string) => void;
 }
 
-export const AllProducts: React.FC<AllProductsProps> = ({ items }) => {
+export const AllProducts: React.FC<AllProductsProps> = ({ items, onMessage }) => {
   return (
     <div className="flow-root">
+      <div>
+        <h2 className="text-2xl font-semibold mb-6">Alle Producten</h2>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 pb-4">
         {items.map((item, index) => (
           <ProductCard
@@ -16,7 +20,7 @@ export const AllProducts: React.FC<AllProductsProps> = ({ items }) => {
             price={item.price}
             media={item.media}
             productId={item.id}
-            onMessage={() => {}}
+            onMessage={onMessage}
           />
         ))}
       </div>
