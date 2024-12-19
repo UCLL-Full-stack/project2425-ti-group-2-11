2f5c6738c1d327c773fe41a3dee88090aec8a015
TuurKlaasenUCLL <tuur.klaasen@student.ucll.be>
2024-12-19 16:35:03 +0100

    added alert if owner wants to change his own role

--- front-end/components/profile/options/owner.tsx
+++ front-end/components/profile/options/owner.tsx
@@ -1,10 +1,24 @@
 import UserService from "@/services/UserService";
-import { Role } from "@/types/types";
+import { Address, Role } from "@/types/types";
+import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
+
+interface ProfileProps {
+    userId?: number;
+    name: string;
+    phoneNumber: string;
+    emailAddress: string;
+    password: string;
+    address: Address;
+    seller: boolean;
+    newsLetter: boolean;
+    role: Role;
+}
 
 const Owner: React.FC = () => {
     const [users, setUsers] = useState<any[]>([]);
     const roles = ["Admin", "Owner", "User"];
+    const [currentUserId, setCurrentUserId] = useState<number>();
 
     const getAllUsers = async () => {
         try {
@@ -17,10 +31,29 @@ const Owner: React.FC = () => {
     };
 
     useEffect(() => {
+            const fetchToken = async () => {
+                const token = localStorage.getItem('token');
+                if (token) {
+                    const decoded = jwtDecode<ProfileProps>(token);
+                    if (decoded && decoded.userId) {
+                        setCurrentUserId(decoded.userId);
+                    }
+                }
+            }
+            fetchToken()
+        }, [])
+
+    useEffect(() => {
         getAllUsers();
     }, []);
 
     const handleChange = (userId: number, newRole: Role) => {
+        if (userId === currentUserId) {
+            const confirmChange = window.confirm("Are you sure you want to change your own role? You will lose access to this page.");
+            if (!confirmChange) {
+                return;
+            }
+        }
         setUsers((prevUsers) =>
             prevUsers.map((user) =>
                 user.id === userId ? { ...user, role: newRole } : user
@@ -30,6 +63,10 @@ const Owner: React.FC = () => {
         UserService.updateUserRole(userId, newRole)
             .then(() => console.log(`Role updated for user ${userId}`))
             .catch((error) => console.error("Failed to update role:", error));
+
+            if (userId === currentUserId) {
+                window.location.reload()
+            }
     };
 
     return (
@@ -37,14 +74,14 @@ const Owner: React.FC = () => {
             {users.map((user) => (
                 <div
                     key={user.id}
-                    className="grid grid-cols-2 items-center gap-2 max-w-80"
+                    className="grid grid-cols-2 items-center gap-2 max-w-80 "
                 >
-                    <p className="pb-2">{user.name}</p>
+                    <p className="pb-2 mt-2">{user.name}</p>
 
                     <select
                         value={user.role || "Role not found"}
                         onChange={(e) => handleChange(user.id, e.target.value as Role)}
-                        className="border p-2 rounded-lg"
+                        className="ml-2 p-1 rounded-lg bg-transparent text-black border-black border-2"
                     >
                         {roles.map((role) => (
                             <option key={role} value={role}>
